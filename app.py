1dbe9b250201b8e866b8abec977a316673561700
mathiasmunene <mathiasmunene@gmail.com>
2025-08-05 23:25:27 +0300

    2nd route added

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ app = Flask(__name__)
 def home():
     return 'Welcome to the Cat App'
 
+@app.route('/cats')
+def get_cats():
+    conn = sqlite3.connect('pet_database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT name FROM cats WHERE owner_id = 1')
+    cats = cursor.fetchall()
+    conn.close()
+    return {'cats': [cat[0] for cat in cats]}
+
+if __name__ == '__main__':
+    app.run(debug=True)
